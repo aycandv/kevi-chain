3b269a8d7954c7127c6f0ff35e7af8bdf03693d9
Aycan Deniz Vit <avit16@ku.edu.tr>
2021-05-03 22:10:09 +0300

    Add function to mine block

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require("crypto-js/sha256")
+
 class Block {
 
     constructor(timestamp, previousHash, hash, data) {
@@ -17,6 +19,18 @@ class Block {
 
     static genesis() {
         return new this("Genesis time", "0", "d312-wr59s", [])
+    }
+
+    static mineBlock(previousBlock, data) {
+        const timestamp = Date.now();
+        const previousHash = previousBlock.hash;
+        const hash = Block.hash(timestamp, previousHash, data);
+
+        return new this(timestamp, previousHash, hash, data);
+    }
+
+    static hash(timestamp, previousHash, data) {
+        return SHA256(`${timestamp}${previousHash}${data}`).toString();
     }
 
 }
